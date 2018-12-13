5e315ddf34e79e3f6218cb4a6bd10678ad1b692d
Adrian Nedelcu <adrian.c.nedelcu@gmail.com>
2018-12-13 22:05:52 +0200

    Inceput input

---
+++ Assets/Scenes/Rocket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput();
+	}
+
+	private void ProcessInput()
+	{
+		if (Input.GetKey(KeyCode.Space)) {
+			print("Space pressed");
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			print("Space pressed");
+		}
+	}
+}
