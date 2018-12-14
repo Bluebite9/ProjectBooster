54c51fed60e0f9be79ca437d11bfef6622eb548c
Adrian Nedelcu <adrian.c.nedelcu@gmail.com>
2018-12-14 16:49:40 +0200

    Miscare + efect de print la coliziune

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -4,26 +4,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.PlayerLoop;
 
-public class Rocket : MonoBehaviour {
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		ProcessInput();
-	}
+    Rigidbody rigidBody;
+    AudioSource audioSource;
 
-	private void ProcessInput()
-	{
-		if (Input.GetKey(KeyCode.Space)) {
-			print("Space pressed");
-		}
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 
-		if (Input.GetKey(KeyCode.A)) {
-			print("Space pressed");
-		}
-	}
+        rigidBody.mass = 0.07f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("Dead");
+                break;
+        }
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            float thrustSpeed = mainThrust * Time.deltaTime;
+
+            rigidBody.AddRelativeForce(Vector3.up * thrustSpeed);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed);
+        }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation
+    }
 }
