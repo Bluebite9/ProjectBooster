41fffb657fe2c00c8e86162d05f7cfc2dd852afc
Adrian Nedelcu <adrian.c.nedelcu@gmail.com>
2018-12-15 20:30:58 +0200

    Level 1 + Lvevel 2

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -1,83 +1,107 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    [SerializeField] float rcsThrust = 100f;
-    [SerializeField] float mainThrust = 100f;
+	[SerializeField] float rcsThrust = 100f;
+	[SerializeField] float mainThrust = 100f;
 
-    Rigidbody rigidBody;
-    AudioSource audioSource;
+	Rigidbody rigidBody;
+	AudioSource audioSource;
 
-    // Use this for initialization
-    void Start()
-    {
-        rigidBody = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();
+	enum State
+	{
+		Alive,
+		Dying,
+		Transcending
+	}
 
-        rigidBody.mass = 0.07f;
-    }
+	private State state = State.Alive;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Thrust();
-        Rotate();
-    }
+	// Use this for initialization
+	void Start()
+	{
+		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 
-    void OnCollisionEnter(Collision collision)
-    {
-        switch (collision.gameObject.tag)
-        {
-            case "Friendly":
-                print("OK");
-                break;
-            case "Fuel":
-                print("Fuel");
-                break;
-            default:
-                print("Dead");
-                break;
-        }
-    }
+		rigidBody.mass = 0.07f;
+	}
 
-    private void Thrust()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            float thrustSpeed = mainThrust * Time.deltaTime;
+	// Update is called once per frame
+	void Update()
+	{
+		if (State.Alive == state)
+		{
+			Thrust();
+			Rotate();
+		}
+	}
 
-            rigidBody.AddRelativeForce(Vector3.up * thrustSpeed);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
-        }
-        else
-        {
-            audioSource.Stop();
-        }
-    }
+	void OnCollisionEnter(Collision collision)
+	{
+		if (state != State.Alive) {
+			return;
+		}
 
-    private void Rotate()
-    {
-        rigidBody.freezeRotation = true; // take manual control of rotation
+		switch (collision.gameObject.tag)
+		{
+			case "Friendly":
+				break;
+			case "Finish":
+				state = State.Transcending;
+				Invoke("LoadNextLevel", 1f);
+				break;
+			default:
+				state = State.Dying;
+				Invoke("LoadFirstLevel", 1f);
+				break;
+		}
+	}
 
-        
-        float rotationSpeed = rcsThrust * Time.deltaTime;
+	private void LoadNextLevel()
+	{
+		SceneManager.LoadScene(1);
+	}
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward * rotationSpeed);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward * rotationSpeed);
-        }
+	private void LoadFirstLevel()
+	{
+		SceneManager.LoadScene(0);
+	}
 
-        rigidBody.freezeRotation = false; // resume physics control of rotation
-    }
+	private void Thrust()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			float thrustSpeed = mainThrust * Time.deltaTime;
+
+			rigidBody.AddRelativeForce(Vector3.up * thrustSpeed);
+			if (!audioSource.isPlaying)
+			{
+				audioSource.Play();
+			}
+		}
+		else
+		{
+			audioSource.Stop();
+		}
+	}
+
+	private void Rotate()
+	{
+		rigidBody.freezeRotation = true; // take manual control of rotation
+
+
+		float rotationSpeed = rcsThrust * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(Vector3.forward * rotationSpeed);
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(-Vector3.forward * rotationSpeed);
+		}
+
+		rigidBody.freezeRotation = false; // resume physics control of rotation
+	}
 }
